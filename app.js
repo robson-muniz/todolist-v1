db3f66890cda30c6c1ba516cc91f5e4f3c01edfc
robson-muniz <robson_muniz@yahoo.com>
2019-09-18 17:29:46 -0300

    Pass data from webpage to the Server.

--- app.js
+++ app.js
@@ -3,43 +3,36 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";            
-            break;
-            case 1:
-            day = "Monday";            
-            break;
-            case 2:
-            day = "Tuesday";            
-            break;
-            case 3:
-            day = "Wednesday";            
-            break;
-            case 4:
-            day = "Thursday";            
-            break;
-            case 5:
-            day = "Friday";            
-            break;
-            case 6:
-            day = "Saturday";            
-            break;                      
-        default:
-            console.log("Error: current day is equal to:" + currentDay); 
-            break;
-    }
     
-    res.render("list", {kindOfDay: day});
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+      
+    
+    res.render("list", {kindOfDay: day, newListItems: items});    
 });
+
+app.post("/", function(req, res){
+
+    var item = req.body.newItem;
+
+    items.push(item);
+    
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
