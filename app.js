c98de003919c19f50129f00524169953bde7b4c5
robson-muniz <robson_muniz@yahoo.com>
2019-09-19 19:02:18 -0300

    Templates vs Layouts.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food",];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,17 +25,35 @@ app.get("/", function(req, res){
     let day = today.toLocaleDateString("en-US", options);
       
     
-    res.render("list", {kindOfDay: day, newListItems: items});    
+    res.render("list", {listTitle: day, newListItems: items});    
 });
 
-app.post("/", function(req, res){
+app.post('/', function(req, res){
+    //post items
+        const item = req.body.newItem;
+        if (req.body.list === "Work") {
+            workItems.push(item)
+            res.redirect("/work")
+        } else {
+            items.push(item);
+            res.redirect("/")
+        }   
+        
+    });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.post("/work", function(req, res){ 
     let item = req.body.newItem;
-
-    items.push(item);
-    
-    res.redirect("/");
-})
+    workItems.push(item);
+    res.redirect("/work");
+}); 
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
