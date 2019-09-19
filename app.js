9bb2cc72a5db41197f45e2c35615568a69b81782
robson-muniz <robson_muniz@yahoo.com>
2019-09-19 12:15:29 -0300

    Add some CSS Style to the Project.

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = ["Buy Food", "Cook Food", "Eat Food",];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
     
-    var today = new Date();
+    let today = new Date();
     
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
       
     
     res.render("list", {kindOfDay: day, newListItems: items});    
@@ -27,7 +29,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
     items.push(item);
     
