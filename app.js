2f3a92c47c88facd9e1055f59882093654537d53
robson-muniz <robson_muniz@yahoo.com>
2019-09-19 20:44:25 -0300

    Node Module Exports, passing Functions and Data between Files.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food",];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food",];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,18 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    
-    let today = new Date();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
 
-    let day = today.toLocaleDateString("en-US", options);
-      
-    
+    const day = date.getDate();
     res.render("list", {listTitle: day, newListItems: items});    
 });
 
@@ -47,7 +38,7 @@ app.get("/work", function(req, res){
 
 app.get("/about", function(req, res){
     res.render("about");
-});
+}); 
 
 app.post("/work", function(req, res){ 
     let item = req.body.newItem;
